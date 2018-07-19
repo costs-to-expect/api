c7ca0f86bcaabc00d12c1de608d5c4bc063c55d5
Dean Blackborough <dean@g3d-development.com>
2018-07-19 18:21:51 +0100

    Options
    * Added options for sub category controller
    * Moved GET and OPTIONS routes out of Auth

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,17 @@ Route::post('login', 'PassportController@login');
 Route::post('register', 'PassportController@register');
 
 Route::group(
-    ['middleware' => 'auth:api'],
+    [],
     function () {
         Route::get('categories', 'CategoryController@index');
         Route::options('categories', 'CategoryController@optionsIndex');
         Route::get('categories/{category_id}', 'CategoryController@show');
         Route::options('categories/{category_id}', 'CategoryController@optionsShow');
+
         Route::get('categories/{category_id}/sub_categories', 'SubCategoryController@index');
+        Route::options('categories/{category_id}/sub_categories', 'SubCategoryController@optionsIndex');
         Route::get('categories/{category_id}/sub_categories/{sub_category_id}', 'SubCategoryController@show');
+        Route::options('categories/{category_id}/sub_categories/{sub_category_id}', 'SubCategoryController@optionsShow');
 
         Route::get('resource_types', 'ResourceTypeController@index');
         Route::get('resource_types/{resource_type_id}', 'ResourceTypeController@show');
@@ -33,6 +36,13 @@ Route::group(
         Route::get('resource_types/{resource_type_id}/resources/{resource_id}/items', 'ItemController@index');
         Route::get('resource_types/{resource_type_id}/resources/{resource_id}/items/{item_id}', 'ItemController@show');
 
+        Route::get('user', 'PassportController@user');
+    }
+);
+
+Route::group(
+    ['middleware' => 'auth:api'],
+    function () {
         Route::get('user', 'PassportController@user');
     }
 );
