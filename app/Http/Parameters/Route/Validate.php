8479e8b0c1accf52ac311e16bc99d1dd75aa9913
Dean Blackborough <dean@g3d-development.com>
2018-10-08 23:46:13 +0100

    Request utility updated, able to define custom error messages
    * Update other classes to use new Request class.
    * Request utility updated, able to define custom error messages.

--- app/Http/Parameters/Route/Validate.php
+++ app/Http/Parameters/Route/Validate.php
@@ -9,6 +9,7 @@ use App\Http\Parameters\Route\Validators\ItemSubCategory;
 use App\Http\Parameters\Route\Validators\Resource;
 use App\Http\Parameters\Route\Validators\ResourceType;
 use App\Http\Parameters\Route\Validators\SubCategory;
+use App\Utilities\Request as UtilityRequest;
 
 /**
  * Validate the set route parameters, redirect to 404 if invalid
@@ -22,35 +23,35 @@ class Validate
     static public function category($category_id)
     {
         if (Category::validate($category_id) === false) {
-            self::return404('Category not found');
+            UtilityRequest::notFound('Category not found');
         }
     }
 
     static public function subCategory($category_id, $sub_category_id)
     {
         if (SubCategory::validate($category_id, $sub_category_id) === false) {
-            self::return404('Sub category not found');
+            UtilityRequest::notFound('Sub category not found');
         }
     }
 
     static public function resourceType($resource_type_id)
     {
         if (ResourceType::validate($resource_type_id) === false) {
-            self::return404('Resource type not found');
+            UtilityRequest::notFound('Resource type not found');
         }
     }
 
     static public function resource($resource_type_id, $resource_id)
     {
         if (Resource::validate($resource_type_id, $resource_id) === false) {
-            self::return404('Resource not found');
+            UtilityRequest::notFound('Resource not found');
         }
     }
 
     static public function item($resource_type_id, $resource_id, $item_id)
     {
         if (Item::validate($resource_type_id, $resource_id, $item_id) === false) {
-            self::return404('Item not found');
+            UtilityRequest::notFound('Item not found');
         }
     }
 
@@ -67,7 +68,7 @@ class Validate
                 $item_category_id
             ) === false
         ) {
-            self::return404('Item category not found');
+            UtilityRequest::notFound('Item category not found');
         }
     }
 
@@ -86,18 +87,7 @@ class Validate
                 $item_sub_category_id
             ) === false
         ) {
-            self::return404('Item sub category not found');
+            UtilityRequest::notFound('Item sub category not found');
         }
-    }
-
-    static protected function return404($message)
-    {
-        response()->json(
-            [
-                'message' => $message
-            ],
-            404
-        )->send();
-        die;
     }
 }
--- app/Utilities/Request.php
+++ app/Utilities/Request.php
@@ -14,45 +14,48 @@ class Request
     /**
      * Return not found, 404
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @param string $message Custom message for error
      */
-    static public function notFound()
+    static public function notFound($message = '')
     {
-        return response()->json(
+        response()->json(
             [
-                'message' => 'Resource not found'
+                'message' => (strlen($message) > 0) ? $message : 'Resource not found'
             ],
             404
-        );
+        )->send();
+        exit;
     }
 
     /**
      * Return a foreign key constraint error, 500
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @param string $message Custom message for error
      */
-    static public function foreignKeyConstraintError()
+    static public function foreignKeyConstraintError($message = '')
     {
-        return response()->json(
+        response()->json(
             [
-                'message' => 'Unable to delete resource, dependant data exists'
+                'message' => (strlen($message) > 0) ? $message : 'Unable to delete resource, dependant data exists'
             ],
             500
-        );
+        )->send();
+        exit;
     }
 
     /**
      * Return a conflict, 409
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @param string $message Custom message for error
      */
-    static public function conflict()
+    static public function conflict($message = '')
     {
-        return response()->json(
+        response()->json(
             [
-                'message' => 'Value already set, conflict'
+                'message' => (strlen($message) > 0) ? $message : 'Value already set, conflict'
             ],
             409
-        );
+        )->send();
+        exit;
     }
 }
