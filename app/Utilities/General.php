34599bc9a53f0c9a76ceef1ca437a28f1d734490
Dean Blackborough <dean@g3d-development.com>
2018-10-09 00:45:47 +0100

    General utility class
    * Working on a general utility class

---
+++ app/Utilities/General.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Utilities;
+
+/**
+ * General utility class
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough 2018
+ * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
+ */
+class General
+{
+    static public function booleanValue()
+    {
+
+    }
+
+    static public function isBooleanValue()
+    {
+
+    }
+}
