0265fff214d98a1c6d1235e0f33403f2ec9dccd1
Dean Blackborough <dean@g3d-development.com>
2018-10-09 23:00:59 +0100

    Added App\Utilities\General utility class
    * Added App\Utilities\General class, includes booleanValue and isBooleanValue

--- app/Utilities/General.php
+++ app/Utilities/General.php
@@ -11,13 +11,40 @@ namespace App\Utilities;
  */
 class General
 {
-    static public function booleanValue()
+    /**
+     * Checks a value and if returns the boolean value after a call to
+     * filter_var($value, FILTER_VALIDATE_BOOLEAN)
+     *
+     * @param $value
+     *
+     * @return bool
+     */
+    static public function booleanValue($value)
     {
+        if (filter_var($value, FILTER_VALIDATE_BOOLEAN) === true) {
+            return true;
+        }
 
+        return false;
     }
 
-    static public function isBooleanValue()
+    /**
+     * Check to see if a value is a valid boolean value, uses filter_var so
+     * true, 1, "true" and "on" are valid positive boolean values, off values
+     * are false, 0, "false" and "off"
+     *
+     * @param $value
+     *
+     * @return bool
+     */
+    static public function isBooleanValue($value)
     {
+        $filtered = filter_var($value, FILTER_VALIDATE_BOOLEAN,FILTER_NULL_ON_FAILURE);
 
+        if ($filtered === true || $filtered === false) {
+            return true;
+        }
+
+        return false;
     }
 }
