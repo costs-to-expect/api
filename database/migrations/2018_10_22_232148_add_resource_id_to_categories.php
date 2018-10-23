634e4353d425b8a2eace2c5f065ad28cf662b9d3
Dean Blackborough <dean@g3d-development.com>
2018-10-23 23:32:42 +0100

    Migration to added resource type id to category table
    * Migration to added resource type id to category table

--- database/migrations/2018_10_22_232148_add_resource_id_to_categories.php
+++ database/migrations/2018_10_22_232148_add_resource_id_to_categories.php
@@ -13,8 +13,13 @@ class AddResourceIdToCategories extends Migration
      */
     public function up()
     {
-        Schema::table('categories', function (Blueprint $table) {
-            //
+        Schema::table('category', function (Blueprint $table) {
+            $table->unsignedBigInteger('resource_type_id')
+                ->after('id')
+                ->default(1);
+            $table->foreign('resource_type_id')
+                ->references('id')
+                ->on('resource_type');
         });
     }
 
@@ -25,8 +30,9 @@ class AddResourceIdToCategories extends Migration
      */
     public function down()
     {
-        Schema::table('categories', function (Blueprint $table) {
-            //
+        Schema::table('category', function (Blueprint $table) {
+            $table->dropForeign('category_resource_type_id_foreign');
+            $table->dropColumn('resource_type_id');
         });
     }
 }
