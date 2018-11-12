1f1fa4750298c6fc1abfbf432c29c8a8f51c1740
Dean Blackborough <dean@g3d-development.com>
2018-11-12 23:27:58 +0000

    New release v1.09.0
    * New release v1.09.0

--- config/api/version.php
+++ config/api/version.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'version'=> '1.08.0',
+    'version'=> '1.09.0',
     'prefix' => 'v1',
-    'release_date' => '2018-11-10',
+    'release_date' => '2018-11-12',
     'changelog' => [
         'api' => '/v1/changelog',
         'markdown' => 'https://github.com/costs-to-expect/api/blob/master/CHANGELOG.md'
