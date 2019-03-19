b3df8c3ca6440117408a18415fd915ba535e298d
Dean Blackborough <dean@g3d-development.com>
2019-03-19 15:04:53 +0000

    Comment updates
    * Updatd PHP doc comments

--- app/Utilities/General.php
+++ app/Utilities/General.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace App\Utilities;
 
 /**
- * General utility class
+ * General utility class, holder of all the methods that do not seem to fit
+ * anywhere else, typically, new classes will spawn from methods within this
+ * class
  *
  * @author Dean Blackborough <dean@g3d-development.com>
  * @copyright Dean Blackborough 2018-2019
@@ -13,7 +15,7 @@ namespace App\Utilities;
 class General
 {
     /**
-     * Checks a value and if returns the boolean value after a call to
+     * Checks a value and see if a boolean is returned after a call to
      * filter_var($value, FILTER_VALIDATE_BOOLEAN)
      *
      * @param $value
@@ -30,11 +32,11 @@ class General
     }
 
     /**
-     * Check to see if a value is a valid boolean value, uses filter_var so
-     * true, 1, "true" and "on" are valid positive boolean values, off values
+     * Check to see if a value is a valid boolean, uses filter_var so true, 1,
+     * "true" and "on" are all valid positive boolean values, off values
      * are false, 0, "false" and "off"
      *
-     * @param $value
+     * @param mixed $value Value to check to see if it is a possible boolean
      *
      * @return bool
      */
