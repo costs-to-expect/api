b2b91f82cf749fbdc84fc084a5c0741cc4f37457
Dean Blackborough <dean@g3d-development.com>
2019-03-27 12:50:14 +0000

    Pagintion parameters tranlations
    * Added translations for pagination parameters

--- config/api/pagination/parameters.php
+++ config/api/pagination/parameters.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 return [
     'offset' => [
         'parameter' => 'offset',
-        'title' => 'Offset',
-        'description' => 'Record offset for pagination',
+        'title' => 'title_offset',
+        'description' => 'description_offset',
         'default' => 0,
         'type' => 'integer',
         'required' => false
     ],
     'limit' => [
         'parameter' => 'limit',
-        'title' => 'Limit',
-        'description' => 'Record limit for pagination',
+        'title' => 'title_limit',
+        'description' => 'description_limit',
         'default' => 10,
         'type' => 'integer',
         'required' => false
--- resources/lang/en/pagination/parameters.php
+++ resources/lang/en/pagination/parameters.php
@@ -3,5 +3,9 @@
 declare(strict_types=1);
 
 return [
+    'title_offset' => 'Offset',
+    'description_offset' => 'Start result for pagination',
 
+    'title_limit' => 'Limit',
+    'description_limit' => 'Number of results to return'
 ];
