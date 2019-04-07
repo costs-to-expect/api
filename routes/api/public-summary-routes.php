b604bb556ae25e4aec4b69b3bf3cc4a9dd19350f
Dean Blackborough <dean@g3d-development.com>
2019-04-07 14:15:26 +0100

    Removed old summary routes
    * Removed old summary routes

--- routes/api/public-summary-routes.php
+++ routes/api/public-summary-routes.php
@@ -23,48 +23,6 @@ Route::group(
             'SummaryItemController@optionsIndex'
         );
 
-
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/years',
-            'SummaryPeriodController@years'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/years',
-            'SummaryPeriodController@optionsYears'
-        );
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/years/{year}',
-            'SummaryPeriodController@year'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/years/{year}',
-            'SummaryPeriodController@optionsYear'
-        );
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/years/{year}/months',
-            'SummaryPeriodController@months'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/years/{year}/months',
-            'SummaryPeriodController@optionsMonths'
-        );
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/years/{year}/months/{month}',
-            'SummaryPeriodController@month'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/years/{year}/months/{month}',
-            'SummaryPeriodController@optionsMonth'
-        );
-
         Route::get(
             'summary/request/access-log/monthly',
             'SummaryRequestController@monthlyAccessLog'
@@ -73,56 +31,6 @@ Route::group(
         Route::options(
             'summary/request/access-log/monthly',
             'SummaryRequestController@optionsMonthlyAccessLog'
-        );
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/tco',
-            'SummaryController@tco'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/tco',
-            'SummaryController@optionsTco'
-        );
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/categories',
-            'SummaryCategoryController@categories'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/categories',
-            'SummaryCategoryController@optionsCategories'
-        );
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/categories/{category_id}',
-            'SummaryCategoryController@category'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/categories/{category_id}',
-            'SummaryCategoryController@optionsCategory'
-        );
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/categories/{category_id}/subcategories',
-            'SummaryCategoryController@subCategories'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/categories/{category_id}/subcategories',
-            'SummaryCategoryController@optionsSubCategories'
-        );
-
-        Route::get(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/categories/{category_id}/subcategories/{sub_category_id}',
-            'SummaryCategoryController@subCategory'
-        );
-
-        Route::options(
-            'summary/resource-types/{resource_type_id}/resources/{resource_id}/categories/{category_id}/subcategories/{sub_category_id}',
-            'SummaryCategoryController@optionsSubCategory'
         );
     }
 );
