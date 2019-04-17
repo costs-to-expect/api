b5fcf1bc02659918d38bd1e03f6ebc52fa1ffb5a
Dean Blackborough <dean@g3d-development.com>
2019-04-17 16:42:36 +0100

    Parameters defined
    * Added the parameter definitions.

--- config/api/resource-type-item/parameters.php
+++ config/api/resource-type-item/parameters.php
@@ -17,6 +17,38 @@ return [
             'description' => 'resource-type-item/parameters.description-include-subcategories',
             'type' => 'boolean',
             'required' => false
+        ],
+        'year' => [
+            "parameter" => "year",
+            "title" => 'resource-type-item/parameters.title-year',
+            "description" => 'resource-type-item/parameters.description-year',
+            "default" => null,
+            "type" => "integer",
+            "required" => false
+        ],
+        'month' => [
+            "parameter" => "month",
+            "title" => 'resource-type-item/parameters.title-month',
+            "description" => 'resource-type-item/parameters.description-month',
+            "default" => null,
+            "type" => "integer",
+            "required" => false
+        ],
+        'category' => [
+            "parameter" => "category",
+            "title" => 'resource-type-item/parameters.title-category',
+            "description" => 'resource-type-item/parameters.description-category',
+            "default" => null,
+            "type" => "string",
+            "required" => false
+        ],
+        'subcategory' => [
+            "parameter" => "subcategory",
+            "title" => 'resource-type-item/parameters.title-subcategory',
+            "description" => 'resource-type-item/parameters.description-subcategory',
+            "default" => null,
+            "type" => "string",
+            "required" => false
         ]
     ],
     'item' => []
