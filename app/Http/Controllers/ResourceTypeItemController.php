d7fc8bbd4971bb889c7e450e407f0fa4784b5b70
Dean Blackborough <dean@g3d-development.com>
2019-04-17 17:36:40 +0100

    Allowed values in the OPTIONS request
    * OPTIONS request now shows allowed values

--- app/Http/Controllers/ResourceTypeItemController.php
+++ app/Http/Controllers/ResourceTypeItemController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Parameters\Get;
 use App\Http\Parameters\Route\Validate;
+use App\Models\Category;
 use App\Models\ResourceTypeItem;
+use App\Models\SubCategory;
 use App\Models\Transformers\ResourceTypeItem as ResourceTypeItemTransformer;
 use App\Utilities\Pagination as UtilityPagination;
 use Illuminate\Http\JsonResponse;
@@ -19,6 +21,8 @@ use Illuminate\Http\Request;
  */
 class ResourceTypeItemController extends Controller
 {
+    private $conditional_get_parameters = [];
+
     /**
      * Return all the items based on the set filter options
      *
@@ -87,14 +91,90 @@ class ResourceTypeItemController extends Controller
     {
         Validate::resourceTypeRoute($resource_type_id);
 
+        $this->conditionalGetParameters(
+            $resource_type_id,
+            Get::parameters([
+                'category',
+            ])
+        );
+
         return $this->generateOptionsForIndex(
             [
                 'description_localisation' => 'route-descriptions.resource_type_item_GET_index',
                 'parameters_config' => 'api.resource-type-item.parameters.collection',
-                'conditionals' => [],
+                'conditionals' => $this->conditional_get_parameters,
                 'pagination' => true,
                 'authenticated' => false
             ]
         );
+    }
+
+    /**
+     * Fetch the conditional GET parameters reasy to be be merged with the
+     * GET parameters data array, useful for defining dynamic values such as
+     * allowed values
+     *
+     * @param integer $resource_type_id
+     * @param array $collection_parameters
+     */
+    private function conditionalGetParameters($resource_type_id, array $collection_parameters)
+    {
+        $this->conditional_get_parameters = [
+            'year' => [
+                'allowed_values' => []
+            ],
+            'month' => [
+                'allowed_values' => []
+            ],
+            'category' => [
+                'allowed_values' => []
+            ],
+            'subcategory' => [
+                'allowed_values' => []
+            ]
+        ];
+
+        for ($i=2013; $i <= intval(date('Y')); $i++) {
+            $this->conditional_get_parameters['year']['allowed_values'][$i] = [
+                'value' => $i,
+                'name' => $i,
+                'description' => trans('item/allowed-values.description-prefix-year') . $i
+            ];
+        }
+
+        for ($i=1; $i < 13; $i++) {
+            $this->conditional_get_parameters['month']['allowed_values'][$i] = [
+                'value' => $i,
+                'name' => date("F", mktime(0, 0, 0, $i, 10)),
+                'description' => trans('item/allowed-values.description-prefix-month') .
+                    date("F", mktime(0, 0, 0, $i, 1))
+            ];
+        }
+
+        (new Category())->paginatedCollection($this->include_private, ['resource_type'=>$resource_type_id])->map(
+            function ($category)
+            {
+                $this->conditional_get_parameters['category']['allowed_values'][$this->hash->encode('category', $category->category_id)] = [
+                    'value' => $this->hash->encode('category', $category->category_id),
+                    'name' => $category->category_name,
+                    'description' => trans('item/allowed-values.description-prefix-category') .
+                        $category->category_name . trans('item/allowed-values.description-suffix-category')
+                ];
+            }
+        );
+
+        if (array_key_exists('category', $collection_parameters) === true) {
+            (new SubCategory())->paginatedCollection($collection_parameters['category'])->map(
+                function ($sub_category)
+                {
+                    $this->conditional_get_parameters['subcategory']['allowed_values'][$this->hash->encode('sub_category', $sub_category->id)] = [
+                        'value' => $this->hash->encode('sub_category', $sub_category->id),
+                        'name' => $sub_category->name,
+                        'description' => trans('item/allowed-values.description-prefix-subcategory') .
+                            $sub_category->name . trans('item/allowed-values.description-suffix-subcategory')
+                    ];
+                }
+            );
+        }
     }
 }
