bed5d4e28b86412e6d1b7d364dcb01f084947438
Dean Blackborough <dean@g3d-development.com>
2019-06-06 16:28:16 +0100

    Sort parameters
    - Added class which will be used to validate sort parameters

---
+++ app/Validators/Request/SortParameters.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Validators\Request;
+
+
+/**
+ * Validate the sort parameters
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough 2018-2019
+ * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
+ */
+class SortParameters
+{
+
+}
