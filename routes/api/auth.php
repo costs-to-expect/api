7544d6ec94b228d81a89a0ee9e18937f9b976ab4
Dean Blackborough <dean@g3d-development.com>
2019-07-07 13:30:40 +0100

    Passport tweaks
    - Minor tweak to passport setup.

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
 Route::post('auth/login', 'PassportController@login')->prefix(Config::get('api.version.prefix'));
+Route::post('auth/user', 'PassportController@user')->prefix(Config::get('api.version.prefix'));
 /**
  * Disabled because there are only currently going to be two users
  */
-//Route::post('auth/register', 'PassportController@register')->prefix(Config::get('api.version.prefix'));
+// Route::post('auth/register', 'PassportController@register')->prefix(Config::get('api.version.prefix'));
