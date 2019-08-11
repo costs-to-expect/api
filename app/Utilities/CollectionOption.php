a253879ee705575aac6ea2aeca43ae6a653f9085
Dean Blackborough <dean@g3d-development.com>
2019-08-11 10:05:28 +0100

    Initial API for the CollectionOption class
    - Initial API for the CollectionOption class.

---
+++ app/Utilities/CollectionOption.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Utilities;
+
+// Need a base OPTION class later
+
+/**
+ * Helper class to generate the output for an OPTIONS request
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough 2018-2019
+ * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
+ */
+class CollectionOption
+{
+    static public function setAuthenticationRequired(bool $status = false)
+    {
+
+    }
+
+    static public function setConditionalParameters(array $parameters)
+    {
+
+    }
+
+    static public function setDescription(string $localisation_path)
+    {
+
+    }
+
+    static public function setPagination(bool $status = false)
+    {
+
+    }
+
+    static public function setPaginationOverride(bool $status = false)
+    {
+
+    }
+
+    static public function setParameters(string $config_path)
+    {
+
+    }
+
+    static public function setSearchable(string $config_path)
+    {
+
+    }
+
+    static public function setSortable(string $config_path)
+    {
+
+    }
+}
