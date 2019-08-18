4a91034da3a80e4fb9ea9ab6be92cee640b6266b
Dean Blackborough <dean@g3d-development.com>
2019-08-18 23:08:50 +0100

    Event and Listener for request errors
    - Base of Event and Listener to send request error emails
    - Added Guzzle

---
+++ app/Listeners/CaptureAndSend.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\RequestError;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CaptureAndSend
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  RequestError  $event
+     * @return void
+     */
+    public function handle(RequestError $event)
+    {
+        if (isset($event->request_error) && count($event->request_error) > 0) {
+            // Send an email
+            
+        }
+    }
+}
