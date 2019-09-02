eec8e05cb35004017d7b8d12d758f6daf6ead966
Dean Blackborough <dean@g3d-development.com>
2019-09-02 23:27:57 +0100

    Minor tweak to request log
    - Minor changes to request log, site returning the odd 500 errors

--- app/Models/RequestLog.php
+++ app/Models/RequestLog.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 use Illuminate\Support\Facades\DB;
 
 /**
  * Request log
  *
+ * @mixin QueryBuilder
  * @author Dean Blackborough <dean@g3d-development.com>
  * @copyright G3D Development Limited 2018-2019
  * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
@@ -37,7 +39,7 @@ class RequestLog extends Model
         array $collection_parameters = []
     )
     {
-        $collection = $this->orderByDesc('created_at');
+        $collection = $this->orderByDesc('id');
 
         if (array_key_exists('source', $collection_parameters) === true) {
             $collection->where('source', '=', $collection_parameters['source']);
