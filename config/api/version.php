20777d3e108da950305404b84379be811ce1796d
Dean Blackborough <dean@g3d-development.com>
2019-09-05 14:02:37 +0100

    Updated CHANGELOG for release
    - Updated the version for release.
    - Updated the changelog.

--- config/api/version.php
+++ config/api/version.php
@@ -3,7 +3,7 @@
 return [
     'version'=> '1.23.0',
     'prefix' => 'v1',
-    'release_date' => '2019-09-xx',
+    'release_date' => '2019-09-05',
     'changelog' => [
         'api' => '/v1/changelog',
         'markdown' => 'https://github.com/costs-to-expect/api/blob/master/CHANGELOG.md'
