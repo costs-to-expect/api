fbfdf988556203434f9347e2aafa5581df33d4cd
Dean Blackborough <dean@g3d-development.com>
2019-09-18 23:01:26 +0100

    Class comments
    - Added a coment to utility classes, reming me to review them periodically.

--- app/Utilities/Hash.php
+++ app/Utilities/Hash.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Config;
 /**
  * Utility hash class to encode and decode strings by type
  *
+ * As with all utility classes, eventually they may be moved into libraries if
+ * they gain more than a few functions and the creation of a library makes
+ * sense.
+ *
  * @author Dean Blackborough <dean@g3d-development.com>
  * @copyright G3D Development Limited 2018-2019
  * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
---
+++ app/Utilities/Header.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Utilities;
+
+/**
+ * Header utility functions.
+ *
+ * As with all utility classes, eventually they may be moved into libraries if
+ * they gain more than a few functions and the creation of a library makes
+ * sense.
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited 2018-2019
+ * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
+ */
+class Header
+{
+    
+}
