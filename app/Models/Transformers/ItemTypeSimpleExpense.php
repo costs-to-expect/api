293b310b88cd669acb2399a5e77ddc976f3dc6e2
Dean Blackborough <dean@g3d-development.com>
2019-09-30 23:06:04 +0100

    Item type transformers
    - Transformer classes for each item type

---
+++ app/Models/Transformers/ItemTypeSimpleExpense.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Models\Transformers;
+
+/**
+ * Transform the data returns from Eloquent into the format we want for the API
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright G3D Development Limited 2018-2019
+ * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
+ */
+class ItemTypeSimpleExpense extends Transformer
+{
+    protected $item;
+
+    public function __construct(array $item)
+    {
+        parent::__construct();
+
+        $this->item = $item;
+    }
+
+    public function toArray(): array
+    {
+        $item = [
+            'id' => $this->hash->item()->encode($this->item['item_id']),
+            'name' => $this->item['item_name'],
+            'description' => $this->item['item_description'],
+            'total' => number_format((float) $this->item['item_total'],2, '.', ''),
+            'effective_date' => $this->item['item_effective_date'],
+            'created' => $this->item['item_created_at']
+        ];
+
+        if (
+            array_key_exists('category_id', $this->item) === true &&
+            array_key_exists('category_name', $this->item) === true
+        ) {
+            $item['category'] = [
+                'id' => $this->hash->category()->encode($this->item['category_id']),
+                'name' => $this->item['category_name'],
+                'description' => $this->item['category_description']
+            ];
+
+            if (
+                array_key_exists('subcategory_id', $this->item) === true &&
+                array_key_exists('subcategory_name', $this->item) === true
+            ) {
+                $item['subcategory'] = [
+                    'id' => $this->hash->subCategory()->encode($this->item['subcategory_id']),
+                    'name' => $this->item['subcategory_name'],
+                    'description' => $this->item['subcategory_description']
+                ];
+            }
+        }
+
+        return $item;
+    }
+}
