3b9a2f82a2901921d8970da67ef21fe978ad9b75
Dean Blackborough <dean@g3d-development.com>
2019-10-14 14:17:23 +0100

    Moved a lang file
    - Moved the pagination lang file.

--- config/api/app/pagination-parameters-including-collection.php
+++ config/api/app/pagination-parameters-including-collection.php
@@ -5,24 +5,24 @@ declare(strict_types=1);
 return [
     'offset' => [
         'parameter' => 'offset',
-        'title' => 'pagination/parameters.title-offset',
-        'description' => 'pagination/parameters.description-offset',
+        'title' => 'app/pagination-parameters-including-collection.title-offset',
+        'description' => 'app/pagination-parameters-including-collection.description-offset',
         'default' => 0,
         'type' => 'integer',
         'required' => false
     ],
     'limit' => [
         'parameter' => 'limit',
-        'title' => 'pagination/parameters.title-limit',
-        'description' => 'pagination/parameters.description-limit',
+        'title' => 'app/pagination-parameters-including-collection.title-limit',
+        'description' => 'app/pagination-parameters-including-collection.description-limit',
         'default' => 10,
         'type' => 'integer',
         'required' => false
     ],
     'collection' => [
         'parameter' => 'collection',
-        'title' => 'pagination/parameters.title-collection',
-        'description' => 'pagination/parameters.description-collection',
+        'title' => 'app/pagination-parameters-including-collection.title-collection',
+        'description' => 'app/pagination-parameters-including-collection.description-collection',
         'default' => false,
         'type' => 'boolean',
         'required' => false
--- config/api/app/pagination-parameters.php
+++ config/api/app/pagination-parameters.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 return [
     'offset' => [
         'parameter' => 'offset',
-        'title' => 'pagination/parameters.title-offset',
-        'description' => 'pagination/parameters.description-offset',
+        'title' => 'app/pagination-parameters.title-offset',
+        'description' => 'app/pagination-parameters.description-offset',
         'default' => 0,
         'type' => 'integer',
         'required' => false
     ],
     'limit' => [
         'parameter' => 'limit',
-        'title' => 'pagination/parameters.title-limit',
-        'description' => 'pagination/parameters.description-limit',
+        'title' => 'app/pagination-parameters.title-limit',
+        'description' => 'app/pagination-parameters.description-limit',
         'default' => 10,
         'type' => 'integer',
         'required' => false
