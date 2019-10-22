187cfbe78e8bb42d68c06fcdc0b2e6bd2a0b9081
Dean Blackborough <dean@g3d-development.com>
2019-10-22 10:35:57 +0100

    Correct reference to localisation files
    - Reference the correct localisation files.
    - Remove resource-type-item parameters config file, no longer used

--- config/api/resource-type-item-type-allocated-expense/parameters.php
+++ config/api/resource-type-item-type-allocated-expense/parameters.php
@@ -6,53 +6,53 @@ return [
     'collection' => [
         'include-categories' => [
             'parameter' => 'include-categories',
-            'title' => 'resource-type-item/parameters.title-include-categories',
-            'description' => 'resource-type-item/parameters.description-include-categories',
+            'title' => 'resource-type-item-type-allocated-expense/parameters.title-include-categories',
+            'description' => 'resource-type-item-type-allocated-expense/parameters.description-include-categories',
             'type' => 'boolean',
             'required' => false
         ],
         'include-subcategories' => [
             'parameter' => 'include-subcategories',
-            'title' => 'resource-type-item/parameters.title-include-subcategories',
-            'description' => 'resource-type-item/parameters.description-include-subcategories',
+            'title' => 'resource-type-item-type-allocated-expense/parameters.title-include-subcategories',
+            'description' => 'resource-type-item-type-allocated-expense/parameters.description-include-subcategories',
             'type' => 'boolean',
             'required' => false
         ],
         'include-unpublished' => [
             'parameter' => 'include-unpublished',
-            'title' => 'resource-type-item/parameters.title-include-unpublished',
-            'description' => 'resource-type-item/parameters.description-include-unpublished',
+            'title' => 'resource-type-item-type-allocated-expense/parameters.title-include-unpublished',
+            'description' => 'resource-type-item-type-allocated-expense/parameters.description-include-unpublished',
             'type' => 'boolean',
             'required' => false
         ],
         'year' => [
             "parameter" => "year",
-            "title" => 'resource-type-item/parameters.title-year',
-            "description" => 'resource-type-item/parameters.description-year',
+            "title" => 'resource-type-item-type-allocated-expense/parameters.title-year',
+            "description" => 'resource-type-item-type-allocated-expense/parameters.description-year',
             "default" => null,
             "type" => "integer",
             "required" => false
         ],
         'month' => [
             "parameter" => "month",
-            "title" => 'resource-type-item/parameters.title-month',
-            "description" => 'resource-type-item/parameters.description-month',
+            "title" => 'resource-type-item-type-allocated-expense/parameters.title-month',
+            "description" => 'resource-type-item-type-allocated-expense/parameters.description-month',
             "default" => null,
             "type" => "integer",
             "required" => false
         ],
         'category' => [
             "parameter" => "category",
-            "title" => 'resource-type-item/parameters.title-category',
-            "description" => 'resource-type-item/parameters.description-category',
+            "title" => 'resource-type-item-type-allocated-expense/parameters.title-category',
+            "description" => 'resource-type-item-type-allocated-expense/parameters.description-category',
             "default" => null,
             "type" => "string",
             "required" => false
         ],
         'subcategory' => [
             "parameter" => "subcategory",
-            "title" => 'resource-type-item/parameters.title-subcategory',
-            "description" => 'resource-type-item/parameters.description-subcategory',
+            "title" => 'resource-type-item-type-allocated-expense/parameters.title-subcategory',
+            "description" => 'resource-type-item-type-allocated-expense/parameters.description-subcategory',
             "default" => null,
             "type" => "string",
             "required" => false
--- config/api/resource-type-item-type-simple-expense/parameters.php
+++ config/api/resource-type-item-type-simple-expense/parameters.php
@@ -6,46 +6,46 @@ return [
     'collection' => [
         'include-categories' => [
             'parameter' => 'include-categories',
-            'title' => 'resource-type-item/parameters.title-include-categories',
-            'description' => 'resource-type-item/parameters.description-include-categories',
+            'title' => 'resource-type-item-type-simple-expense/parameters.title-include-categories',
+            'description' => 'resource-type-item-type-simple-expense/parameters.description-include-categories',
             'type' => 'boolean',
             'required' => false
         ],
         'include-subcategories' => [
             'parameter' => 'include-subcategories',
-            'title' => 'resource-type-item/parameters.title-include-subcategories',
-            'description' => 'resource-type-item/parameters.description-include-subcategories',
+            'title' => 'resource-type-item-type-simple-expense/parameters.title-include-subcategories',
+            'description' => 'resource-type-item-type-simple-expense/parameters.description-include-subcategories',
             'type' => 'boolean',
             'required' => false
         ],
         'year' => [
             "parameter" => "year",
-            "title" => 'resource-type-item/parameters.title-year',
-            "description" => 'resource-type-item/parameters.description-year',
+            "title" => 'resource-type-item-type-simple-expense/parameters.title-year',
+            "description" => 'resource-type-item-type-simple-expense/parameters.description-year',
             "default" => null,
             "type" => "integer",
             "required" => false
         ],
         'month' => [
             "parameter" => "month",
-            "title" => 'resource-type-item/parameters.title-month',
-            "description" => 'resource-type-item/parameters.description-month',
+            "title" => 'resource-type-item-type-simple-expense/parameters.title-month',
+            "description" => 'resource-type-item-type-simple-expense/parameters.description-month',
             "default" => null,
             "type" => "integer",
             "required" => false
         ],
         'category' => [
             "parameter" => "category",
-            "title" => 'resource-type-item/parameters.title-category',
-            "description" => 'resource-type-item/parameters.description-category',
+            "title" => 'resource-type-item-type-simple-expense/parameters.title-category',
+            "description" => 'resource-type-item-type-simple-expense/parameters.description-category',
             "default" => null,
             "type" => "string",
             "required" => false
         ],
         'subcategory' => [
             "parameter" => "subcategory",
-            "title" => 'resource-type-item/parameters.title-subcategory',
-            "description" => 'resource-type-item/parameters.description-subcategory',
+            "title" => 'resource-type-item-type-simple-expense/parameters.title-subcategory',
+            "description" => 'resource-type-item-type-simple-expense/parameters.description-subcategory',
             "default" => null,
             "type" => "string",
             "required" => false
