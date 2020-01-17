4e8f2a757e84a37f544cd495eb1ae39417fdb3f5
Dean Blackborough <dean@g3d-development.com>
2020-01-17 13:45:22 +0000

    Working on new landing page
    - Base for the new landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,64 +1,168 @@
-@extends('layouts.app')
 
-@section('content')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <meta name="description" content="The Costs to Expect API. Open source REST API focused on budgeting and forecasting">
+    <meta name="author" content="Dean Blackborough">
+    <meta name="copyright" content="Dean Blackborough 2018-{{ date('Y') }}">
+    <link href="{{ asset('node_modules/open-iconic/font/css/open-iconic-bootstrap.css') }}" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('images/theme/favicon.ico') }}">
+    <link rel="icon" sizes="16x16 32x32 64x64" href="{{ asset('images/theme/favicon.ico') }}">
+    <link rel="icon" type="image/png" sizes="196x196" href="{{ asset('images/theme/favicon-192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/theme/favicon-180.png') }}">
+    <meta name="msapplication-TileColor" content="#FFFFFF">
+    <meta name="msapplication-TileImage" content="{{ asset('images/theme/favicon-144.png') }}">
+    <title>Costs to Expect API</title>
 
-    <main role="main" class="inner cover">
-        <h1 class="cover-heading text-center">Costs to Expect API</h1>
-        @if ($maintenance === true)
-        <div class="alert alert-info mt-4" role="alert">
-            The Costs to Expect API is down for maintenance, we should be back online soon.
+    <link href="https://app.costs-to-expect.com/css/app.css" rel="stylesheet">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 preview text-center">
+            <p>Our App is in the alpha stage, the beta is coming soon.</p>
         </div>
-        @endif
+    </div>
+    <div class="row">
+        <div class="col-xl-2 col-lg-3 col-md-3 container-left d-none d-sm-none d-md-block">
+            <div class="row">
+                <div class="col-12">
+                    <div class="logo">
+                        <a href="/dashboard"><img src="https://app.costs-to-expect.com/images/theme/logo-190.png" width="64" height="64" alt="Costs to Expect Logo"
+                                                  title="Back to the dashboard"/><span class="d-none">C</span>osts to Expect.com</a>
+                    </div>
 
-        <p class="lead mt-4">
-            Costs to Expect is a service which primarily focuses on the tracking
-            and forecasting of expenses; it grew from an offhand comment I made
-            to my wife before we had our first child,
-            <em>"There is no way the average child costs £250,000 to raise!"</em>
-        </p>
+                    <ul class="menu list-unstyled">
+                        <li><strong>Costs to Expect</strong>
+                            <ul class="submenu">
+                                <li><a class="nav-link  active " href="/"><span class="oi oi-shield" title="Our privacy policy" aria-hidden="true"></span>The API</a></li>
+                                <li><a class="nav-link " href="https://github.com/costs-to-expect/api/blob/master/CHANGELOG.md"><span class="oi oi-script" title="Our changelog" aria-hidden="true"></span>Changelog</a></li>
+                                <li><a class="nav-link" href="https://www.costs-to-expect.com" title="The Costs to Expect Website"><span class="oi oi-monitor" title="The Costs to Expect Website" aria-hidden="true"></span>The Website</a></li>
+                                <li><a class="nav-link" href="https://app.costs-to-expect.com" title="The Costs to Expect App"><span class="oi oi-monitor" title="The Costs to Expect App" aria-hidden="true"></span>The App</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-10 col-lg-9 col-md-9 container-right">
+            <nav class="navbar navbar-light d-md-none">
+                <a class="navbar-brand" href="/dashboard">
+                    <img src="https://app.costs-to-expect.com/images/theme/logo-100.png" width="32" height="32" alt="Costs to Expect Logo"
+                         title="Back to the dashboard"/><span class="d-none">C</span>osts to Expect.com
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#toggleNavbar" aria-controls="navbarTogglerApp"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        <p>
-            Medium-term the service will not be limited to expenses, the project
-            grew from a personal expenses tracking project, so it makes sense to
-            focus initially on expenses.
-        </p>
+                <div class="collapse navbar-collapse mt-1" id="toggleNavbar">
+                    <div class="row">
+                        <div class="col-12">
+                            <ul class="menu list-unstyled">
+                                <li><strong>Costs to Expect</strong>
+                                    <ul class="submenu">
+                                        <li><a class="nav-link  active " href="/"><span class="oi oi-shield" title="Our privacy policy" aria-hidden="true"></span>The API</a></li>
+                                        <li><a class="nav-link " href="https://github.com/costs-to-expect/api/blob/master/CHANGELOG.md"><span class="oi oi-script" title="Our changelog" aria-hidden="true"></span>Changelog</a></li>
+                                        <li><a class="nav-link" href="https://www.costs-to-expect.com" title="The Costs to Expect Website"><span class="oi oi-monitor" title="The Costs to Expect Website" aria-hidden="true"></span>The Website</a></li>
+                                        <li><a class="nav-link" href="https://app.costs-to-expect.com" title="The Costs to Expect App"><span class="oi oi-monitor" title="The Costs to Expect App" aria-hidden="true"></span>The App</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
 
-        <p>
-            There are three parts to the service, two Open Source and one soon to
-            be commercial product.
-        </p>
+            <div class="row page-intro">
+                <div class="col-12">
+                    <div class="intro">
+                        <div class="icon">
+                            <img src="https://app.costs-to-expect.com/images/theme/header-icon-dashboard.png" width="50" height="50" alt="Icon" title="Our privacy policy: Costs to Expect.com" />
+                        </div>
+                        <div class="welcome">
+                            Welcome to Costs to Expect
+                        </div>
+                        <div class="title">
+                            <h1>The Costs to Expect API</h1>
+                        </div>
 
-        <ul class="mb-5 mt-5">
-            <li>The API is the backbone of the service; it is
-                <a href="https://github.com/costs-to-expect/api/blob/master/LICENSE">Open Source</a>
-                and available under the MIT license.</li>
-            <li>The <a href="https://www.costs-to-expect.com">Website</a> is a
-                long-term personal project; my wife and I
-                are tracking the expenses to raise our two
-                children to adulthood, 18. The Website is
-                <a href="https://github.com/costs-to-expect/website/blob/master/LICENSE">Open Source</a>
-                and available under the MIT license.</li>
-            <li>The <a href="https://www.costs-to-expect.com">website</a> is a
-                long-term personal project; my wife and I are tracking the expenses
-                to raise our two children to adulthood, 18. The Website is Open
-                Source and available under the MIT license.</li>
-            <li>The <a href="https://app.costs-to-expect.com">App</a> is the
-                beginnings of our commercial product; we are hoping to
-                have an alpha ready for the end of the year with betas in early
-                2020.</li>
-        </ul>
+                        <hr />
+                    </div>
+                </div>
+            </div>
 
-        <p class="text-center mt-3">
-            <a href="/v2" class="btn btn-md btn-primary">Access the API</a>
-            <a href="https://www.costs-to-expect.com" class="btn btn-md btn-primary">The Website</a>
-        </p>
-        <p class="text-center">
-            <a href="https://github.com/costs-to-expect" class="btn btn-md btn-primary">The API on GitHub</a>
-            <a href="https://app.costs-to-expect.com" class="btn btn-dm btn-primary">The app</a>
-        </p>
 
-        <p class="text-center"><small>Latest release: {{ $version }} ({{ $date }})</small></p>
+            <div class="row">
+                <div class="col-12">
 
-    </main>
+                    <h2>Our privacy policy <small>(19th December 2019)</small></h2>
 
-@endsection
+                    <p>Your privacy is important to us. It is our
+                        policy to respect your privacy regarding any information
+                        we may collect from you across our website,
+                        <a href="https://app.costs-to-expect.com">https://app.costs-to-expect.com</a>,
+                        and other sites we own and operate.</p>
+
+                    <p>We only ask for personal information when we truly need
+                        it to provide a service to you. We collect it by fair
+                        and lawful means, with your knowledge and consent.
+                        We also let you know why we’re collecting it and how
+                        it will be used.</p>
+
+                    <p>We only retain collected information for as long as
+                        necessary to provide you with your requested service.
+                        What data we store, we’ll protect within commercially
+                        acceptable means to prevent loss and theft, as well
+                        as unauthorised access, disclosure, copying, use or
+                        modification.</p>
+
+                    <p>We don’t share any personally identifying information
+                        publicly or with third-parties, except when required
+                        to by law.</p>
+
+                    <p>Our website may link to external sites that are not
+                        operated by us. Please be aware that we have no
+                        control over the content and practices of these sites,
+                        and cannot accept responsibility or liability for
+                        their respective privacy policies.</p>
+
+                    <p>You are free to refuse our request for your
+                        personal information, with the understanding that we
+                        may be unable to provide you with some of your desired
+                        services.</p>
+
+                    <p>Your continued use of our website will be regarded as
+                        acceptance of our practices around privacy and personal
+                        information. If you have any questions about how we
+                        handle user data and personal information, feel free
+                        to contact us.</p>
+
+                    <p>This policy is effective as of <strong>19th December 2019</strong>.</p>
+
+                    <p><strong>Dean Blackborough</strong></p>
+
+                </div>
+            </div>
+
+            <div class="row footer">
+                <div class="col-12">
+                    <p class="text-center text-muted">
+                        Copyright © <a href="https://www.deanblackborough.com">Dean Blackborough 2018 - {{ date('Y') }}</a><br>
+                        <a href="https://app.costs-to-expect.com">The App</a> |
+                        <a href="https://status.costs-to-expect.com/">Status</a>
+                        <small>v1.00.0 released [Approaching]</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="{{ asset('node_modules/jquery/dist/jquery.js') }}" defer></script>
+<script src="{{ asset('node_modules/popper.js/dist/umd/popper.js') }}" defer></script>
+<script src="{{ asset('node_modules/bootstrap/dist/js/bootstrap.js') }}" defer></script>
+</body>
+</html>
