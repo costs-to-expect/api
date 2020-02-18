7eb828f1c88157bd33465d736de7b01ad92d1bd2
Dean Blackborough <dean@g3d-development.com>
2020-02-18 22:09:23 +0000

    Correct migration
    - Migration incorrect, should use dropColumn.

--- database/migrations/2020_02_18_082837_remove_effective_date.php
+++ database/migrations/2020_02_18_082837_remove_effective_date.php
@@ -14,7 +14,7 @@ class RemoveEffectiveDate extends Migration
     public function up()
     {
         Schema::table('item_type_simple_expense', function (Blueprint $table) {
-            $table->removeColumn('effective_date');
+            $table->dropColumn('effective_date');
         });
     }
 
