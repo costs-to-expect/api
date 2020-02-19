6c26801d1cd5354ce524d351dcc72ba689554133
Dean Blackborough <dean@g3d-development.com>
2020-02-19 12:23:53 +0000

    Add item type
    - Added migration which inserts the new item type

---
+++ database/migrations/2020_02_19_121906_add_simple_item_type.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSimpleItemType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('INSERT INTO `item_type` (`name`, `description`, `created_at`) VALUES ("simple-item", "Track a collection", NOW())');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // No down, we are not removing this type
+    }
+}
