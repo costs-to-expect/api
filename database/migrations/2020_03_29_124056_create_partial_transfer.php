4c7568232c28e5d653acee5c163e2ba5fb9639b1
Dean Blackborough <dean@g3d-development.com>
2020-03-29 13:11:33 +0100

    Add partial transfer table
    - Added migration for the partial transfer table

---
+++ database/migrations/2020_03_29_124056_create_partial_transfer.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartialTransfer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('partial_transfer', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('resource_type_id');
+            $table->unsignedBigInteger('from');
+            $table->unsignedBigInteger('to');
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedTinyInteger('percentage');
+            $table->unsignedBigInteger('transferred_by');
+            $table->timestamps();
+            $table->foreign('resource_type_id')->references('id')->on('resource_type');
+            $table->foreign('from')->references('id')->on('resource');
+            $table->foreign('to')->references('id')->on('resource');
+            $table->foreign('item_id')->references('id')->on('item');
+            $table->foreign('transferred_by')->references('id')->on('users');
+            $table->unique(['resource_type_id', 'from', 'item_id'], 'unique_partial_transfer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('partial_transfer');
+    }
+}
