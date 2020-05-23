af72334321606b1c2d7f156db15e1876354dd975
Dean Blackborough <dean@g3d-development.com>
2020-05-23 22:40:11 +0100

    Release
    - Updated release notes for new point release.

--- config/api/app/version.php
+++ config/api/app/version.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'version'=> '2.10.3',
+    'version'=> '2.10.4',
     'prefix' => 'v2',
-    'release_date' => '2020-05-09',
+    'release_date' => '2020-05-23',
     'changelog' => [
         'api' => '/v2/changelog',
         'markdown' => 'https://github.com/costs-to-expect/api/blob/master/CHANGELOG.md'
