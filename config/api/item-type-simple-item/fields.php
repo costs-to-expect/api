20d8e294ace44a95e325eaa05194b34513ea7faa
Dean Blackborough <dean@g3d-development.com>
2020-05-25 22:27:19 +0100

    Name is required
    - Name is a required field.

--- config/api/item-type-simple-item/fields.php
+++ config/api/item-type-simple-item/fields.php
@@ -11,7 +11,7 @@ return [
         'validation' => [
             'max-length' => 255
         ],
-        'required' => false
+        'required' => true
     ],
     'description' => [
         'field' => 'description',
