8c006540ed0214cda303a98f4f6f0eabcb7286d5
Dean Blackborough <dean@g3d-development.com>
2020-07-11 22:24:14 +0100

    Refactoring
    - Removed metod we are no longer using.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\ResourceType;
 use App\Models\ResourceTypeAccess;
 use App\Request\Hash;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
@@ -53,40 +52,5 @@ class Controller extends BaseController
             $this->user_id = auth('api')->user()->id;
             $this->permitted_resource_types = (new ResourceTypeAccess())->permittedResourceTypes($this->user_id);
         }
-    }
-
-    /**
-     * Generate and return the options response
-     *
-     * @param array $verbs Verb arrays
-     * @param integer $http_status_code, defaults to 200
-     *
-     * @return JsonResponse
-     */
-    protected function optionsResponse(array $verbs, $http_status_code = 200): JsonResponse
-    {
-        $options = [
-            'verbs' => [],
-            'http_status_code' => $http_status_code,
-            'headers' => [
-                'Content-Security-Policy' => 'default-src \'none\'',
-                'Strict-Transport-Security' => 'max-age=31536000;',
-                'Content-Type' => 'application/json',
-                'Content-Language' => app()->getLocale(),
-                'Referrer-Policy' => 'strict-origin-when-cross-origin',
-                'X-Content-Type-Options' => 'nosniff'
-            ]
-        ];
-
-        foreach ($verbs as $verb => $detail) {
-            $options['verbs'][$verb] = $detail;
-        }
-
-        response()->json(
-            $options['verbs'],
-            $options['http_status_code'],
-            $options['headers']
-        )->send();
-        exit;
     }
 }
