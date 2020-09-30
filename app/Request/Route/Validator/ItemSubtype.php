98f7d15747505b68d3cdf0b701ae9b187d602a3a
Dean Blackborough <dean@g3d-development.com>
2020-09-30 12:35:00 +0100

    Route validation
    - Added route validation for the item subtype route

---
+++ app/Request/Route/Validator/ItemSubtype.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Request\Route\Validator;
+
+use App\Models\ResourceTypeAccess;
+
+/**
+ * Validate the route params to an item subtype
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough 2018-2020
+ * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
+ */
+class ItemSubtype
+{
+    /**
+     * @param string|int $item_type_id
+     * @param string|int $item_subtype_id
+     *
+     * @return boolean
+     */
+    public static function existsToUserForViewing($item_type_id, $item_subtype_id): bool
+    {
+        return !(
+            $item_type_id === 'nill' ||
+            $item_subtype_id === 'nill' ||
+            (new ResourceTypeAccess())->itemSubTypeExistsToUser($item_type_id, $item_subtype_id) === false
+        );
+    }
+}
