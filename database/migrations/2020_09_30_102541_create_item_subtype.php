0cd8228fdf0bde8582fdee90c106a03418e39207
Dean Blackborough <dean@g3d-development.com>
2020-09-30 11:51:05 +0100

    Create item subtype
    - Create the item subtype table

---
+++ database/migrations/2020_09_30_102541_create_item_subtype.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemSubtype extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_subtype', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->tinyIncrements('id');
+            $table->unsignedTinyInteger('item_type_id');
+            $table->string('name', 255);
+            $table->string('description');
+            $table->timestamps();
+            $table->index('name');
+            $table->foreign('item_type_id')->references('id')->on('item_type');
+        });
+
+        DB::statement('INSERT INTO `item_subtype` (`item_type_id`, `name`, `description`, `created_at`) VALUES (1, "default", "Subtypes not supported for type", NOW())');
+        DB::statement('INSERT INTO `item_subtype` (`item_type_id`, `name`, `description`, `created_at`) VALUES (2, "default", "Subtypes not supported for type", NOW())');
+        DB::statement('INSERT INTO `item_subtype` (`item_type_id`, `name`, `description`, `created_at`) VALUES (3, "default", "Subtypes not supported for type", NOW())');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_subtype');
+    }
+}
