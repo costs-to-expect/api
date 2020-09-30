7ca4f23507cb1f27feac9d4dd542ec63e077f285
Dean Blackborough <dean@g3d-development.com>
2020-09-30 11:18:03 +0100

    Remove unique index
    - Remove the unique index on the item category assignment tables

---
+++ database/migrations/2020_09_30_095217_remove_unique_key_item_categories.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveUniqueKeyItemCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('item_category', function (Blueprint $table) {
+            $table->dropForeign('item_category_category_id_foreign');
+            $table->dropForeign('item_category_item_id_foreign');
+            $table->dropUnique(['item_id', 'category_id']);
+
+            $table->foreign('item_id')->references('id')->on('item');
+            $table->foreign('category_id')->references('id')->on('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('item_category', function (Blueprint $table) {
+            $table->dropForeign('item_category_category_id_foreign');
+            $table->dropForeign('item_category_item_id_foreign');
+
+            $table->foreign('item_id')->references('id')->on('item');
+            $table->foreign('category_id')->references('id')->on('category');
+            $table->unique(['item_id', 'category_id']);
+        });
+    }
+}
