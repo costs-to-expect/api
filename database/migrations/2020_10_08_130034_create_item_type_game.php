db84782196d83688f35f58b8f4ae25c269ffbc71
Dean Blackborough <dean@g3d-development.com>
2020-10-08 14:13:20 +0100

    Game type migration
    - Added the initial migration for the game item type

---
+++ database/migrations/2020_10_08_130034_create_item_type_game.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemTypeGame extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_type_game', function (Blueprint $table) {
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('item_id');
+            $table->string('name'); // Game and Datetime??
+            $table->string('description')->nullable();
+            $table->longText('game');
+            $table->longText('statistics');
+            $table->timestamps();
+            $table->foreign('item_id')->references('id')->on('item');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_type_game');
+    }
+}
