ea0e063b1793be5e26df7aedd47f69c56f1ce1f6
Dean Blackborough <dean@g3d-development.com>
2020-10-08 10:13:23 +0100

    Release date
    - Set release date

--- config/api/app/version.php
+++ config/api/app/version.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'version'=> 'v2.15.0',
     'prefix' => 'v2',
-    'release_date' => '2020-10-09',
+    'release_date' => '2020-10-08',
     'changelog' => [
         'api' => '/v2/changelog',
         'markdown' => 'https://github.com/costs-to-expect/api/blob/master/CHANGELOG.md'
