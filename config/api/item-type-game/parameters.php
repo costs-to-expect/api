e6e20488f035cb6491d211f9c68f505b67a6b046
Dean Blackborough <dean@g3d-development.com>
2020-11-07 13:59:43 +0000

    Complete
    - Config for complete parameter

--- config/api/item-type-game/parameters.php
+++ config/api/item-type-game/parameters.php
@@ -3,6 +3,14 @@
 declare(strict_types=1);
 
 return [
-    'collection' => [],
+    'collection' => [
+        'complete' => [
+            'parameter' => 'complete',
+            'title' => 'item-type-game/parameters.title-complete',
+            'description' => 'item-type-game/parameters.description-complete',
+            'type' => 'boolean',
+            'required' => false
+        ]
+    ],
     'item' => []
 ];
--- resources/lang/en/item-type-game/summary-parameters.php
+++ resources/lang/en/item-type-game/summary-parameters.php
@@ -3,9 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'title-resources' => 'Resource',
-    'description-resources' => 'Return the games summary grouped by game',
-
     'title-complete' => 'Show complete games',
     'description-complete' => 'Optionally, only show complete games',
 ];
