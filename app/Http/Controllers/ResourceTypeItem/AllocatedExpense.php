335ef580954de1b557edd4590fd0c0a1662b6d93
Dean Blackborough <dean@g3d-development.com>
2020-11-18 12:34:12 +0000

    Item type classes
    - Adding additional item type classes

---
+++ app/Http/Controllers/ResourceTypeItem/AllocatedExpense.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\ResourceTypeItem;
+
+use App\Models\Transformers\ResourceTypeItem\AllocatedExpense as Transformer;
+use App\Response\Cache;
+use Illuminate\Http\JsonResponse;
+
+class AllocatedExpense extends Item
+{
+    public function response(): JsonResponse
+    {
+        $this->fetchAllRequestParameters(
+            new \App\Entity\Item\AllocatedExpense()
+        );
+
+        $this->cache_control->setTtlOneWeek();
+
+        $cache_collection = new Cache\Collection();
+        $cache_collection->setFromCache($this->cache_control->getByKey(request()->getRequestUri()));
+
+        if (
+            $this->cache_control->isRequestCacheable() === false ||
+            $cache_collection->valid() === false
+        ) {
+            $model = new \App\Models\ResourceTypeItem\AllocatedExpense();
+
+            $total = $model->totalCount(
+                $this->resource_type_id,
+                $this->request_parameters,
+                $this->search_parameters,
+                $this->filter_parameters
+            );
+
+            $pagination_parameters = $this->pagination_parameters($total);
+
+            $items = $model->paginatedCollection(
+                $this->resource_type_id,
+                $pagination_parameters['offset'],
+                $pagination_parameters['limit'],
+                $this->request_parameters,
+                $this->search_parameters,
+                $this->filter_parameters,
+                $this->sort_fields
+            );
+
+            $collection = array_map(
+                static function ($item) {
+                    return (new Transformer($item))->asArray();
+                },
+                $items
+            );
+
+            $cache_collection->create(
+                $total,
+                $collection,
+                $pagination_parameters,
+                $this->headers(
+                    $pagination_parameters,
+                    count($items),
+                    $total,
+                    $collection
+                )
+            );
+            $this->cache_control->putByKey(request()->getRequestUri(), $cache_collection->content());
+        }
+
+        return response()->json($cache_collection->collection(), 200, $cache_collection->headers());
+    }
+}
---
+++ app/Http/Controllers/ResourceTypeItem/Item.php
@@ -0,0 +1,101 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers\ResourceTypeItem;
+
+use App\Request\Parameter\Filter;
+use App\Request\Parameter\Request;
+use App\Request\Parameter\Search;
+use App\Request\Parameter\Sort;
+use App\Response\Cache;
+use App\Response\Header\Headers;
+use App\Response\Pagination as UtilityPagination;
+use Illuminate\Http\JsonResponse;
+
+abstract class Item
+{
+    protected int $resource_type_id;
+
+    protected bool $permitted_user;
+
+    protected ?int $user_id;
+
+    protected Cache\Control $cache_control;
+
+    protected array $request_parameters;
+    protected array $search_parameters;
+    protected array $filter_parameters;
+    protected array $sort_fields;
+
+    public function __construct(
+        int $resource_type_id,
+        bool $permitted_user,
+        ?int $user_id
+    )
+    {
+        $this->resource_type_id = $resource_type_id;
+        $this->permitted_user = $permitted_user;
+        $this->user_id = $user_id;
+
+        $this->cache_control = new Cache\Control(
+            $this->permitted_user,
+            $this->user_id
+        );
+    }
+
+    abstract public function response(): JsonResponse;
+
+    protected function headers(
+        array $pagination_parameters,
+        int $count,
+        int $total,
+        array $collection
+    ): array
+    {
+        $headers = new Headers();
+        $headers
+            ->collection($pagination_parameters, $count, $total)
+            ->addCacheControl($this->cache_control->visibility(), $this->cache_control->ttl())
+            ->addETag($collection)
+            ->addSearch(Search::xHeader())
+            ->addSort(Sort::xHeader())
+            ->addParameters(Request::xHeader())
+            ->addFilters(Filter::xHeader());
+
+        return $headers->headers();
+    }
+
+    protected function fetchAllRequestParameters(
+        \App\Entity\Item\Item $entity
+    ): void
+    {
+        $this->request_parameters = Request::fetch(
+            array_keys($entity->resourceTypeRequestParameters()),
+            $this->resource_type_id
+        );
+
+        $this->search_parameters = Search::fetch(
+            $entity->resourceTypeSearchParameters()
+        );
+
+        $this->filter_parameters = Filter::fetch(
+            $entity->resourceTypeFilterParameters()
+        );
+
+        $this->sort_fields = Sort::fetch(
+            $entity->resourceTypeSortParameters()
+        );
+    }
+
+    protected function pagination_parameters(int $total): array
+    {
+        $pagination = new UtilityPagination(request()->path(), $total);
+        return $pagination
+            ->allowPaginationOverride(false)
+            ->setSearchParameters($this->search_parameters)
+            ->setSortParameters($this->sort_fields)
+            ->setParameters($this->request_parameters)
+            ->setFilteringParameters($this->filter_parameters)
+            ->parameters();
+    }
+}
