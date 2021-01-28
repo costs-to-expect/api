4097e660564200e2b7693cf194c132db37f59e5c
Dean Blackborough <dean@g3d-development.com>
2021-01-28 14:05:30 +0000

    Tests
    - Adding authentication tests

--- tests/Feature/Http/Controllers/AuthenticationTest.php
+++ tests/Feature/Http/Controllers/AuthenticationTest.php
@@ -2,21 +2,75 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    use RefreshDatabase;
-
     /** @test */
-    public function registration_shows ()
+    public function registration_errors_with_no_payload()
     {
-        $this->refreshDatabase();
-
-        $response = $this->post(route('auth.register'), []);
+        $response = $this->post(
+            route('auth.register'),
+            []
+        );
 
         $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function registration_errors_with_no_email()
+    {
+        $response = $this->post(
+            route('auth.register'),
+            [
+                'name' => $this->faker->name
+            ]
+        );
+
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function registration_errors_with_no_name()
+    {
+        $response = $this->post(
+            route('auth.register'),
+            [
+                'email' => $this->faker->email
+            ]
+        );
+
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function registration_errors_with_bad_email()
+    {
+        $response = $this->post(
+            route('auth.register'),
+            [
+                'name' => $this->faker->name,
+                'email' => 'email.email.com'
+            ]
+        );
+
+        $response->assertStatus(422);
+    }
+
+    /** @test */
+    public function registration_success(): void
+    {
+        $response = $this->post(
+            route('auth.register'),
+            [
+                'name' => $this->test_account_name,
+                'email' => $this->test_account_email
+            ]
+        );
+
+        print_r($response->json());
+        die;
+
+        $response->assertStatus(201);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,27 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, Withfaker;
+
+    protected string $test_account_name;
+    protected string $test_account_email;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->test_account_name = $this->faker->name;
+        $this->test_account_email = $this->faker->email;
+
+        $result = DB::select(DB::raw("SHOW TABLES LIKE 'users';"));
+
+        if (!count($result)) {
+            $this->artisan('migrate:fresh');
+        }
+    }
 }
