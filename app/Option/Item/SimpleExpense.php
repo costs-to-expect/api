52cf6105cbb21a087aa2dcd5ef0a7b44208b2c39
Dean Blackborough <dean@g3d-development.com>
2022-01-24 23:53:16 +0000

    Refactoring
    - Call the config files directory, no need for the class methods

--- app/Option/Item/SimpleExpense.php
+++ app/Option/Item/SimpleExpense.php
@@ -3,17 +3,17 @@ declare(strict_types=1);
 
 namespace App\Option\Item;
 
-use App\ItemType\SimpleExpense\Item;
 use App\Option\Response;
+use Illuminate\Support\Facades\Config as LaravelConfig;
 
 class SimpleExpense extends Response
 {
     public function create()
     {
-        $item = new Item();
+        $base_path = 'api.item-type-simple-expense';
 
         $get = new \App\Method\GetRequest();
-        $this->verbs['GET'] = $get->setParameters($item->itemRequestParameters())
+        $this->verbs['GET'] = $get->setParameters(LaravelConfig::get($base_path . '.parameters.item', []))
             ->setAuthenticationStatus($this->permissions['view'])
             ->setDescription('route-descriptions.item_GET_show')
             ->option();
@@ -25,7 +25,7 @@ class SimpleExpense extends Response
             ->option();
 
         $patch = new \App\Method\PatchRequest();
-        $this->verbs['PATCH'] = $patch->setFields($item->patchFields())
+        $this->verbs['PATCH'] = $patch->setFields(LaravelConfig::get($base_path . '.fields', []))
             ->setDescription('route-descriptions.item_PATCH')
             ->setAuthenticationStatus($this->permissions['manage'])
             ->setAuthenticationRequirement(true)
