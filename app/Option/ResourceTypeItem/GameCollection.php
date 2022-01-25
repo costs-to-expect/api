2bb040de15939467910863e67cd60fbb6ee78d6b
Dean Blackborough <dean@g3d-development.com>
2022-01-25 15:12:41 +0000

    Refactoring
    - Updated option classes to not call the pointless class

--- app/Option/ResourceTypeItem/GameCollection.php
+++ app/Option/ResourceTypeItem/GameCollection.php
@@ -3,21 +3,21 @@ declare(strict_types=1);
 
 namespace App\Option\ResourceTypeItem;
 
-use App\ItemType\Game\Item;
 use App\Option\Response;
+use Illuminate\Support\Facades\Config as LaravelConfig;
 
 class GameCollection extends Response
 {
     public function create()
     {
-        $item = new Item();
+        $base_path = 'api.resource-type-item-type-game';
 
         $get = new \App\Method\GetRequest();
-        $this->verbs['GET'] = $get->setSortableParameters($item->resourceTypeSortParameters())->
-            setSearchableParameters($item->resourceTypeSearchParameters())->
-            setFilterableParameters($item->resourceTypeFilterParameters())->
+        $this->verbs['GET'] = $get->setSortableParameters(LaravelConfig::get($base_path . '.sortable', []))->
+            setSearchableParameters(LaravelConfig::get($base_path . '.searchable', []))->
+            setFilterableParameters(LaravelConfig::get($base_path . '.filterable', []))->
             setPaginationStatus(true)->
-            setParameters($item->resourceTypeRequestParameters())->
+            setParameters(LaravelConfig::get($base_path . '.parameters.collection', []))->
             setDynamicParameters($this->allowed_parameters)->
             setDescription('route-descriptions.resource_type_item_GET_index')->
             setAuthenticationStatus($this->permissions['view'])->
