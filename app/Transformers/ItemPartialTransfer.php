f6f2003289b9546528b8def56846ba72f3757697
Dean Blackborough <dean@g3d-development.com>
2022-04-18 11:20:05 +0100

    Partial transfers
    - Updated the partial transfers collection.
    - Updated the route file, switched additional routes to names routes

--- app/Transformers/ItemPartialTransfer.php
+++ app/Transformers/ItemPartialTransfer.php
@@ -12,18 +12,27 @@ class ItemPartialTransfer extends Transformer
 {
     public function format(array $to_transform): void
     {
+        $resource_type_id = $this->hash->itemPartialTransfer()->encode($to_transform['id']);
+        $from_resource_id = $this->hash->resource()->encode($to_transform['from_resource_id']);
+        $to_resource_id = $this->hash->resource()->encode($to_transform['to_resource_id']);
+        $item_id = $this->hash->item()->encode($to_transform['item_item_id']);
+        $user_id = $this->hash->user()->encode($to_transform['user_id']);
+
         $this->transformed = [
-            'id' => $this->hash->itemPartialTransfer()->encode($to_transform['id']),
+            'id' => $resource_type_id,
             'from' => [
-                'id' => $this->hash->resource()->encode($to_transform['from_resource_id']),
+                'uri' => route('resource.show', ['resource_type_id' => $resource_type_id, 'resource_id' => $from_resource_id], false),
+                'id' => $from_resource_id,
                 'name' => $to_transform['from_resource_name'],
             ],
             'to' => [
-                'id' => $this->hash->resource()->encode($to_transform['to_resource_id']),
+                'uri' => route('resource.show', ['resource_type_id' => $resource_type_id, 'resource_id' => $to_resource_id], false),
+                'id' => $to_resource_id,
                 'name' => $to_transform['to_resource_name'],
             ],
             'item' => [
-                'id' => $this->hash->item()->encode($to_transform['item_item_id']),
+                'uri' => route('item.show', ['resource_type_id' => $resource_type_id, 'resource_id' => $from_resource_id, 'item_id' => $item_id], false),
+                'id' => $item_id,
                 'name' => $to_transform['item_name'],
                 'description' => $to_transform['item_description']
             ],
@@ -31,7 +40,8 @@ class ItemPartialTransfer extends Transformer
             'transferred' => [
                 'at' => $to_transform['created_at'],
                 'user' => [
-                    'id' => $this->hash->user()->encode($to_transform['user_id']),
+                    'uri' => route('permitted-user.show', ['resource_type_id', $resource_type_id, 'permitted_user_id', $user_id], false),
+                    'id' => $user_id,
                     'name' => $to_transform['user_name']
                 ]
             ]
