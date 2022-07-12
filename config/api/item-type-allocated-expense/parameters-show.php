8fd67e598c61ec3d76b98fb86f724d09d9b67567
Dean Blackborough <dean@g3d-development.com>
2022-07-12 14:05:29 +0100

    Resource file and content
    - Added lang file for parameters-show.
    - Updated language in lang file to mention expenses
    - Added the include-players parameter

--- config/api/item-type-allocated-expense/parameters-show.php
+++ config/api/item-type-allocated-expense/parameters-show.php
@@ -5,15 +5,15 @@ declare(strict_types=1);
 return [
     'include-categories' => [
         'parameter' => 'include-categories',
-        'title' => 'item-type-allocated-expense/parameters.title-include-categories',
-        'description' => 'item-type-allocated-expense/parameters.description-include-categories',
+        'title' => 'item-type-allocated-expense/parameters-show.title-include-categories',
+        'description' => 'item-type-allocated-expense/parameters-show.description-include-categories',
         'type' => 'boolean',
         'required' => false
     ],
     'include-subcategories' => [
         'parameter' => 'include-subcategories',
-        'title' => 'item-type-allocated-expense/parameters.title-include-subcategories',
-        'description' => 'item-type-allocated-expense/parameters.description-include-subcategories',
+        'title' => 'item-type-allocated-expense/parameters-show.title-include-subcategories',
+        'description' => 'item-type-allocated-expense/parameters-show.description-include-subcategories',
         'type' => 'boolean',
         'required' => false
     ]
--- config/api/item-type-game/parameters.php
+++ config/api/item-type-game/parameters.php
@@ -3,6 +3,13 @@
 declare(strict_types=1);
 
 return [
+    'include-players' => [
+        'parameter' => 'include-players',
+        'title' => 'item-type-game/parameters.title-include-players',
+        'description' => 'item-type-game/parameters.description-include-players',
+        'type' => 'boolean',
+        'required' => false
+    ],
     'complete' => [
         'parameter' => 'complete',
         'title' => 'item-type-game/parameters.title-complete',
