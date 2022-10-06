6cba19092cf6f4103dfc788795b38bc567882472
Dean Blackborough <dean@g3d-development.com>
2022-10-06 15:14:58 +0100

    Config file referencing wrong lang file
    - Config file not referencing the correct language file for show parameters

--- config/api/resource-type/parameters-show.php
+++ config/api/resource-type/parameters-show.php
@@ -6,14 +6,14 @@ return [
     'include-resources' => [
         'field' => 'include-resources',
         'title' => 'resource-type/parameters-show.title-include-resources',
-        'description' => 'resource-type/parameters.description-include-resources',
+        'description' => 'resource-type/parameters-show.description-include-resources',
         'type' => 'boolean',
         'required' => false
     ],
     'include-permitted-users' => [
         'field' => 'include-permitted-users',
         'title' => 'resource-type/parameters-show.title-include-permitted-users',
-        'description' => 'resource-type/parameters.description-include-permitted-users',
+        'description' => 'resource-type/parameters-show.description-include-permitted-users',
         'type' => 'boolean',
         'required' => false
     ]
