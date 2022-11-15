d66ecf9757b0d6fcd1b345f8bac9132af5a4849f
Dean Blackborough <dean@g3d-development.com>
2022-11-15 14:42:31 +0000

    Release
    - Set the version and release date

--- config/api/app/version.php
+++ config/api/app/version.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
-    'version'=> 'v3.06.1',
+    'version'=> 'v3.07.0',
     'prefix' => 'v3',
-    'release_date' => '2022-10-31',
+    'release_date' => '2022-11-15',
     'changelog' => [
         'api' => '/v3/changelog',
         'markdown' => 'https://github.com/costs-to-expect/api/blob/master/CHANGELOG.md'
