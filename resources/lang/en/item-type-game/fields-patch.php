715e206d926f4915b48238b45a37024a94e62178
Dean Blackborough <dean@g3d-development.com>
2022-12-19 15:41:16 +0000

    Config file corrections
    - Config file corrections

--- resources/lang/en/item-type-game/fields-patch.php
+++ resources/lang/en/item-type-game/fields-patch.php
@@ -3,15 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'title-name' => 'Game name',
-    'description-name' => 'Enter a name for the item',
-
-    'title-description' => 'Game description',
-    'description-description' => 'Enter a description for this game',
-
-    'title-players' => 'Game players',
-    'description-players' => 'JSON containing the player details',
-
     'title-game' => 'Game',
     'description-game' => 'JSON containing the game details',
 
