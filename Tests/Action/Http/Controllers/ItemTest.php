ce239f99ec8fb26466f0a8b7778bbc71d6b53c76
Dean Blackborough <dean@g3d-development.com>
2023-05-24 15:45:19 +0100

    Item tests
    - Working on item tests

---
+++ Tests/Action/Http/Controllers/ItemTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers;
+
+use Tests\TestCase;
+
+final class ItemTest extends TestCase
+{
+    /** @test */
+    public function createAllocatedExpenseItemSuccess(): void
+    {
+        //$response->assertStatus(422);
+    }
+}
