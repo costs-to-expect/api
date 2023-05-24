d13e4f7743d12c4648d795f4eab470f31b0c868a
Dean Blackborough <dean@g3d-development.com>
2023-05-24 22:27:29 +0100

    Namespace
    - Corrected the namespace

--- Tests/Action/Http/Controllers/ItemTest.php
+++ Tests/Action/Http/Controllers/ItemTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers;
+namespace Tests\Action\Http\Controllers;
 
 use Tests\TestCase;
 
--- Tests/Action/Http/Controllers/ResourceTest.php
+++ Tests/Action/Http/Controllers/ResourceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers;
+namespace Tests\Action\Http\Controllers;
 
 use App\HttpRequest\Hash;
 use App\Models\ResourceType;
