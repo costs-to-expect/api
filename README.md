bab7bc84a34c2b4073327a811594cb1e1a438025
Dean Blackborough <dean@g3d-development.com>
2023-05-26 16:04:57 +0100

    Tests
    - Working on tests for budget item and budget collection
    - Added the missing schema for allocated expense
    - Updated the README

--- README.md
+++ README.md
@@ -293,7 +293,7 @@ our local test suite is as complete as the Postman request tests.
 | Category            | 11 Tests | Non yet* |
 | Currency            | Non yet* | Non yet* |
 | ItemCategory        | Non yet* | Non yet* |
-| Item                | 45 Tests | Non yet* |
+| Item                | 45 Tests | 4 Tests  |
 | ItemData            | Non yet* | Non yet* |
 | ItemLog             | Non yet* | Non yet* |
 | ItemPartialTransfer | Non yet* | Non yet* |
@@ -306,7 +306,7 @@ our local test suite is as complete as the Postman request tests.
 | Resource            | 24 Tests | Non yet* |
 | ResourceType        | 23 Tests | 11 Tests |
 | Subcategory         | 12 Tests | Non yet* |
-| **Total tests**     | **154**  | **22**   |
+| **Total tests**     | **154**  | **26**   |
 
 *Non yet does not mean there are no tests, it just means there are no PHPUnit tests. There are over 2000 tests in 
 a private Postman collection, I'm slowing transferring them locally and expanding the test suite.
--- Tests/View/Http/Controllers/ItemTest.php
+++ Tests/View/Http/Controllers/ItemTest.php
@@ -23,7 +23,7 @@ final class ItemTest extends TestCase
 
         $response->assertStatus(200);
 
-        //$this->assertProvidedJsonMatchesDefinedSchema($response->content(), 'api/schema/options/allocated-expense.json');
+        $this->assertProvidedJsonMatchesDefinedSchema($response->content(), 'api/schema/options/allocated-expense.json');
     }
 
     /** @test */
@@ -45,5 +45,45 @@ final class ItemTest extends TestCase
         $response->assertStatus(200);
 
         $this->assertProvidedJsonMatchesDefinedSchema($response->content(), 'api/schema/options/allocated-expense-collection.json');
+    }
+
+    /** @test */
+    public function optionsRequestForBudgetItem(): void
+    {
+        $this->actingAs(User::find(1));
+        $resource_type_id = $this->createBudgetResourceType();
+        $resource_id = $this->createBudgetResource($resource_type_id);
+        $item_id = $this->createBudgetItem($resource_type_id, $resource_id);
+
+        $response = $this->fetchOptionsForItem([
+            'resource_type_id' => $resource_type_id,
+            'resource_id' => $resource_id,
+            'item_id' => $item_id
+        ]);
+
+        $response->assertStatus(200);
+
+        //$this->assertProvidedJsonMatchesDefinedSchema($response->content(), 'api/schema/options/budget.json');
+    }
+
+    /** @test */
+    public function optionsRequestForBudgetItemCollection(): void
+    {
+        $this->actingAs(User::find(1));
+        $resource_type_id = $this->createBudgetResourceType();
+        $resource_id = $this->createBudgetResource($resource_type_id);
+
+        $this->createBudgetItem($resource_type_id, $resource_id);
+        $this->createBudgetItem($resource_type_id, $resource_id);
+        $this->createBudgetItem($resource_type_id, $resource_id);
+
+        $response = $this->fetchOptionsForItemCollection([
+            'resource_type_id' => $resource_type_id,
+            'resource_id' => $resource_id
+        ]);
+
+        $response->assertStatus(200);
+
+        //$this->assertProvidedJsonMatchesDefinedSchema($response->content(), 'api/schema/options/budget-collection.json');
     }
 }
