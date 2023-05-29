04b93f81473f9b68ce9fff2cf09183a7d5a6b331
Dean Blackborough <dean@g3d-development.com>
2023-05-29 12:38:29 +0100

    Budget tests
    - Added budget and budget pro collection tests

--- Tests/View/Http/Controllers/ItemTest.php
+++ Tests/View/Http/Controllers/ItemTest.php
@@ -277,6 +277,158 @@ final class ItemTest extends TestCase
      * @runInSeparateProcess
      * @preserveGlobalState disabled
      */
+    public function budgetItemCollectionPagination(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetResourceType();
+        $resource_id = $this->createBudgetResource($resource_type_id);
+
+        $this->createBudgetItem($resource_type_id, $resource_id);
+        $this->createBudgetItem($resource_type_id, $resource_id);
+        $this->createBudgetItem($resource_type_id, $resource_id);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'offset'=>0,
+            'limit'=> 2
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Offset', 0);
+        $response->assertHeader('X-Limit', 2);
+        $response->assertHeader('X-Link-Previous', "");
+        $response->assertHeader('X-Link-Next', "/v3/resource-types/{$resource_type_id}/resources/{$resource_id}/items?offset=2&limit=2");
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function budgetItemCollectionSearchName(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetResourceType();
+        $resource_id = $this->createBudgetResource($resource_type_id);
+
+        $this->createBudgetItem($resource_type_id, $resource_id);
+        $this->createBudgetItem($resource_type_id, $resource_id, ['name' => 'find-me']);
+        $this->createBudgetItem($resource_type_id, $resource_id);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'search'=>'name:find-me'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Search', 'name:find-me');
+        $response->assertHeader('X-Count', 1);
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function budgetItemCollectionSortAmount(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetResourceType();
+        $resource_id = $this->createBudgetResource($resource_type_id);
+
+        $this->createBudgetItem($resource_type_id, $resource_id, ['amount' => '10000.15']);
+        $this->createBudgetItem($resource_type_id, $resource_id, ['amount' => '110000.27']);
+        $this->createBudgetItem($resource_type_id, $resource_id, ['amount' => '6.15']);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'sort'=>'amount:asc'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Sort', 'amount:asc');
+        $this->assertEquals('6.15', $response->json()[0]['amount']);
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function budgetItemCollectionSortCreated(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetResourceType();
+        $resource_id = $this->createBudgetResource($resource_type_id);
+
+        $this->createBudgetItem($resource_type_id, $resource_id);
+        $this->createBudgetItem($resource_type_id, $resource_id);
+        sleep(1); // ensure the created_at timestamps are different
+        $this->createBudgetItem($resource_type_id, $resource_id, ['name' => 'created-last']);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'sort'=>'created:desc'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Sort', 'created:desc');
+        $this->assertEquals('created-last', $response->json()[0]['name']);
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
     public function budgetItemCollectionSortName(): void
     {
         $this->actingAs(User::find(1));
@@ -346,6 +498,195 @@ final class ItemTest extends TestCase
         ]);
 
         $response->assertStatus(200);
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetProItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function budgetProItemCollectionPagination(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetProResourceType();
+        $resource_id = $this->createBudgetProResource($resource_type_id);
+
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'offset'=>0,
+            'limit'=> 2
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Offset', 0);
+        $response->assertHeader('X-Limit', 2);
+        $response->assertHeader('X-Link-Previous', "");
+        $response->assertHeader('X-Link-Next', "/v3/resource-types/{$resource_type_id}/resources/{$resource_id}/items?offset=2&limit=2");
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetProItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function budgetProItemCollectionSearchName(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetProResourceType();
+        $resource_id = $this->createBudgetProResource($resource_type_id);
+
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+        $this->createBudgetProItem($resource_type_id, $resource_id, ['name' => 'find-me']);
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'search'=>'name:find-me'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Search', 'name:find-me');
+        $response->assertHeader('X-Count', 1);
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetProItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function budgetProItemCollectionSortAmount(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetProResourceType();
+        $resource_id = $this->createBudgetProResource($resource_type_id);
+
+        $this->createBudgetProItem($resource_type_id, $resource_id, ['amount' => '10000.15']);
+        $this->createBudgetProItem($resource_type_id, $resource_id, ['amount' => '110000.27']);
+        $this->createBudgetProItem($resource_type_id, $resource_id, ['amount' => '6.15']);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'sort'=>'amount:asc'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Sort', 'amount:asc');
+        $this->assertEquals('6.15', $response->json()[0]['amount']);
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetProItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function budgetProItemCollectionSortCreated(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetProResourceType();
+        $resource_id = $this->createBudgetProResource($resource_type_id);
+
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+        sleep(1); // ensure the created_at timestamps are different
+        $this->createBudgetProItem($resource_type_id, $resource_id, ['name' => 'created-last']);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'sort'=>'created:desc'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Sort', 'created:desc');
+        $this->assertEquals('created-last', $response->json()[0]['name']);
+
+        foreach ($response->json() as $item) {
+            try {
+                $json = json_encode($item, JSON_THROW_ON_ERROR);
+            } catch (\JsonException $e) {
+                $this->fail('Unable to encode the JSON string');
+            }
+
+            $this->assertJsonMatchesBudgetProItemSchema($json);
+        }
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function budgetProItemCollectionSortName(): void
+    {
+        $this->actingAs(User::find(1));
+
+        $resource_type_id = $this->createBudgetProResourceType();
+        $resource_id = $this->createBudgetProResource($resource_type_id);
+
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+        $this->createBudgetProItem($resource_type_id, $resource_id, ['name' => 'AAAAAAAAAAAA']);
+        $this->createBudgetProItem($resource_type_id, $resource_id);
+
+        $response = $this->fetchItemCollection([
+            $resource_type_id,
+            $resource_id,
+            'sort'=>'name:asc'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertHeader('X-Sort', 'name:asc');
+        $this->assertEquals('AAAAAAAAAAAA', $response->json()[0]['name']);
 
         foreach ($response->json() as $item) {
             try {
