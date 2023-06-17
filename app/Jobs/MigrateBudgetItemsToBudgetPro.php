3dcafa944dce6545af913f7225bc11a9e857628e
Dean Blackborough <dean@g3d-development.com>
2023-06-17 15:02:42 +0100

    *** Interim commit ***
    - Working on the migrate job

--- app/Jobs/MigrateBudgetItemsToBudgetPro.php
+++ app/Jobs/MigrateBudgetItemsToBudgetPro.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Notification;
 use Throwable;
 
@@ -29,7 +30,59 @@ class MigrateBudgetItemsToBudgetPro implements ShouldQueue
 
     public function handle()
     {
+        // Check to see if there is a budget resource type
+        $resource_type = $this->fetchResourceType($this->user_id, 'budget');
 
+        if (count($resource_type) === 0) {
+            $this->fail(new \Exception('Budget Pro migration failed, no budget resource type found for user id:' . $this->user_id));
+        }
+
+        if (count($resource_type) > 1) {
+            $this->fail(new \Exception('Budget Pro migration failed, there appears to be more than one free budget, something has going wrong somewhere for user id:' . $this->user_id));
+        }
+
+        $budget_resource_type_id = $resource_type[0]['id'];
+
+        // Check to see if there is a budget-pro resource type
+        $resource_type = $this->fetchResourceType($this->user_id, 'budget-pro');
+
+        if (count($resource_type) === 0) {
+            $this->fail(new \Exception('Budget Pro migration failed, no budget resource type found for user id:' . $this->user_id));
+        }
+
+        if (count($resource_type) > 1) {
+            $this->fail(new \Exception('Budget Pro migration failed, there appears to be more than one free budget, something has going wrong somewhere for user id:' . $this->user_id));
+        }
+
+        $budget_pro_resource_type_id = $resource_type[0]['id'];
+
+
+        // Check to see if there is a budget resource
+
+
+
+        // Check to see if there are items
+        // Check to see if there is a budget pro resource
+        // Copy over the resource data
+        // Copy over the items
+    }
+
+    private function fetchResourceType(int $user_id, string $item_type): array
+    {
+       return DB::select('
+            SELECT 
+                `resource_type`.`id`
+            FROM `resource_type` 
+            INNER JOIN `permitted_user` ON 
+                `resource_type`.`id` = `permitted_user`.`resource_type_id` AND 
+                `permitted_user`.`user_id` = ? 
+            INNER JOIN `resource_type_item_type` ON 
+                `resource_type`.`id` = `resource_type_item_type`.`resource_type_id`
+            INNER JOIN `item_type` ON 
+                `resource_type_item_type`.`item_type_id` = `item_type`.`id`
+            WHERE 
+                `item_type`.`name` = ?;
+        ', [$user_id, $item_type]);
     }
 
     public function failed(Throwable $exception)
