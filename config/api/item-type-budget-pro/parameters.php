3506f618c90f4fb73f505c82c485fe8434b2ef1e
Dean Blackborough <dean@g3d-development.com>
2023-08-08 19:14:51 +0100

    New params
    - Added include-disabled and include-enable params for Budget pro collection, not implemented yet.

--- config/api/item-type-budget-pro/parameters.php
+++ config/api/item-type-budget-pro/parameters.php
@@ -2,4 +2,21 @@
 
 declare(strict_types=1);
 
-return [];
+return [
+    'include-disabled' => [
+        'parameter' => 'include-disabled',
+        'title' => 'item-type-budget-pro/parameters.title-include-disabled',
+        'description' => 'item-type-budget-pro/parameters.description-include-disabled',
+        "default" => false,
+        'type' => 'boolean',
+        'required' => false
+    ],
+    'include-enabled' => [
+        'parameter' => 'include-enabled',
+        'title' => 'item-type-budget-pro/parameters.title-include-enabled',
+        'description' => 'item-type-budget-pro/parameters.description-include-enabled',
+        "default" => true,
+        'type' => 'boolean',
+        'required' => false
+    ],
+];
---
+++ resources/lang/en/item-type-budget-pro/parameters.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title-include-disabled' => 'Include disabled budget items?',
+    'description-include-disabled' => 'Optionally include any disabled budget items',
+
+    'title-include-enabled' => 'Include enabled budget items? [DEFAULT TRUE]',
+    'description-include-enabled' => 'Optionally control whether enabled budgets items are included, defaults to true',
+];
