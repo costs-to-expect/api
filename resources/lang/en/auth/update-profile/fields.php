f7494d9584c2c2f422f834a5321bff4e14b58d27
Dean Blackborough <dean@g3d-development.com>
2023-10-13 14:49:03 +0100

    Fix options request
    - The options requests was incorrect for update profile

--- resources/lang/en/auth/update-profile/fields.php
+++ resources/lang/en/auth/update-profile/fields.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 return [
+    'title-name' => 'Name',
+    'description-name' => 'Enter your name',
+
     'title-email' => 'Email',
     'description-email' => 'Enter your email address',
-
-    'title-password' => 'Password',
-    'description-password' => 'Enter your password'
 ];
