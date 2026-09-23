1a8ca2b3ffc36718ff9857804ccd0f589b1bf11d
Dean Blackborough <dean@g3d-development.com>
2026-09-23 16:31:08 +0200

    Updated the changelog and version

--- config/api/app/version.php
+++ config/api/app/version.php
@@ -3,13 +3,13 @@
 declare(strict_types=1);
 
 return [
-    'version'=> 'v3.20.0',
+    'version'=> 'v3.21.0',
     'prefix' => 'v3',
-    'release_date' => '2026-09-22',
+    'release_date' => '2026-09-23',
     'changelog' => [
         'api' => '/v3/changelog',
         'markdown' => 'https://github.com/costs-to-expect/api/blob/master/CHANGELOG.md'
     ],
     'readme' => 'https://github.com/costs-to-expect/api/blob/master/README.md',
-    'documentation' => 'https://postman.costs-to-expect.com/?version=latest'
+    'documentation' => '/docs/Overview.html'
 ];
